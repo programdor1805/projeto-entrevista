f771a9fc034d7ba2ddd3b89057281db4438a8d2d
programdor1805 <tiago_alves1805@hotmail.com>
2022-06-14 19:53:05 -0300

    Add files via upload

---
+++ CalculoJuros/Controllers/Git/ShowmethecodeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculoJuros.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShowmethecodeController
+    {
+        public ShowmethecodeController()
+        {
+        }
+
+        [HttpGet]
+        public string RetornEnderecoProjetoGit()
+        {
+            return "https://github.com/programdor1805/projeto-entrevista";
+        }
+
+    }
+}
